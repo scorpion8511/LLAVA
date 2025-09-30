3851ead61ce4a37ad7843d5079cd6510d1226db3
scorpion8511 <dewan8511@gmail.com>
2025-09-30 19:51:15 +0900

    Fix padding image preprocessing for patch CSV

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -27,25 +27,20 @@ def expand2square(pil_img, background_color):
 
 def process_images(images, image_processor, model_cfg):
     image_aspect_ratio = getattr(model_cfg, "image_aspect_ratio", None)
-    new_images = []
     if image_aspect_ratio == 'pad':
-        for image in images:
-            image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
-            image = image_processor.preprocess(
+        images = [
+            expand2square(
                 image,
-                return_tensors='pt',
-                padding=True,
-            )['pixel_values'][0]
-            new_images.append(image)
-    else:
-        return image_processor(
-            images,
-            return_tensors='pt',
-            padding=True,
-        )['pixel_values']
-    if all(x.shape == new_images[0].shape for x in new_images):
-        new_images = torch.stack(new_images, dim=0)
-    return new_images
+                tuple(int(x * 255) for x in image_processor.image_mean),
+            )
+            for image in images
+        ]
+
+    return image_processor(
+        images=images,
+        return_tensors='pt',
+        padding=True,
+    )['pixel_values']
 
 
 def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None):
